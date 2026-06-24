deb6dc7eea81d4c2e4580b0defa1d777a3da48da
Cora Albers <cralbers3@gmail.com>
2026-06-23 22:24:06 -0400

    r executable and resultant graph

--- manual/linear_regression_r.r
+++ manual/linear_regression_r.r
@@ -1,31 +1,35 @@
-install.packages("ggplot2")
+# install.packages("ggplot2")
 library(ggplot2)
 
 
 # Ask user for inputs and save to variables 
-filename <- readline(prompt = "Enter filename: ")
-x = readline(prompt = "Enter column name for x values: ")
-y = readline(prompt = "Enter column name for y values: ")
+cat("Enter filename: ")
+filename <- readLines(con = "stdin", n = 1)
+cat("Enter column name for x values: ")
+x <- readLines(con = "stdin", n = 1)
+cat("Enter column name for y values: ")
+y <- readLines(con = "stdin", n = 1)
+
 
 # import csv dataset
 df <- read.csv(filename)
 
-# make a scatter plot of Salary (y) as a fx of YearsExperience (x)
-plot(df$x, df$y, col="purple")
+# # make a scatter plot of Salary (y) as a fx of YearsExperience (x)
+# plot(df$x, df$y, col="purple")
 
 # fit a linear regression model to the data (salary as a fx of years of experience)
-model <- lm(y ~ x, data=df)
+model <- lm(df[[y]] ~ df[[x]])
 
 
 # graph the raw data and the calculated regression line
 ggplot() +
-    geom_point(aes(x=df$x, y=df$y), color='purple') +
-    geom_line(aes(x=df$x, y=predict(model, newdata=df)), color='red') +
-    ggtitle("Salary vs Experience") +
-    xlab("Years of Experience") +
-    ylab("Salary") +
+    geom_point(aes(x=df[[x]], y=df[[y]]), color='purple') +
+    geom_line(aes(x=df[[x]], y=predict(model, newdata=df)), color='red') +
+    ggtitle(paste(y, "vs", x)) +
+    xlab(x) +
+    ylab(y) +
     theme_bw(base_size=16)
-
+ggsave("linear_regression_r_output.png")
 
 # evaluate the model's performance 
 summary(model)
